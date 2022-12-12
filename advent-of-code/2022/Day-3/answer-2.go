2fada5d7fc8cba3ab88577aea9c6c6b722dc741f
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-12-12 20:50:27 +0100

    Update answer-2.go

--- advent-of-code/2022/Day-3/answer-2.go
+++ advent-of-code/2022/Day-3/answer-2.go
@@ -8,6 +8,22 @@ import (
 	"strings"
 )
 
+func getDifferent(arr1 []string, arr2 []string, arr3 []string) []string {
+	result := []string{}
+	
+	for _, value := range arr1 {
+		isContain := strings.Contains(strings.Join(arr2, ""), value)
+		isContain2 := strings.Contains(strings.Join(arr3, ""), value)
+		isPrev := strings.Contains(strings.Join(result, ""), value)
+		
+		if isContain && isPrev == false && isContain2 {
+			result = append(result, value)
+		}
+	}
+	
+	return result
+}
+
 func main() {
 	// parse data
 	content, err := ioutil.ReadFile("input.txt")
@@ -18,65 +34,37 @@ func main() {
 	var pattern string = `\r?\n`
 	regexObj := regexp.MustCompile(pattern)
 	data := regexObj.Split(dataStr, -1)
+	mainObj := [][]string{}
 	
-	// logic
-	var score int = 0
-	points := map[string]int {
-		"X": 1, 
-		"Y": 2, 
-		"Z": 3, 
-	}
-	status := map[string]string {
-		"X": "lost",
-		"Y": "draw",
-		"Z": "win",
-		"win": "A", 
-		"draw": "B", 
-		"lost": "C",
-	}
-	
-	
+	i := -1
+	looped := -1
 	for _, value := range data {
-		items := strings.Split(value, "")
-		oponent := items[0]
-		roundType := status[items[2]]
-		
-		if roundType == "draw" {
-			score += 3 
-
-			switch oponent {
-				case "A":
-					score += points["X"]
-				case "B":
-					score += points["Y"]
-				case "C":
-					score += points["Z"]
-			}
+		i++
+		if (i%3) == 0 {
+			looped++
 		}
 		
-		if roundType == "win" {
-			score += 6
-			switch oponent {
-				case "A":
-					score += points["Y"]
-				case "B":
-					score += points["Z"]
-				case "C":
-					score += points["X"]
-			}
-		}	
-		
-		if roundType == "lost" {
-			score += 0
-			switch oponent {
-				case "A":
-					score += points["Z"]
-				case "B":
-					score += points["X"]
-				case "C":
-					score += points["Y"]
-			}
+		if looped > len(mainObj)-1 {
+			val := []string{value}
+			mainObj = append(mainObj, val)
+		} else {
+			mainObj[looped] = append(mainObj[looped], value)
 		}
 	} 
-	fmt.Println(score)
+	
+	// logic
+	var result int = 0
+	alphabet :=  []string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+	
+	for _, arr := range mainObj {
+		arr1 := strings.Split(arr[0], "")
+		arr2 := strings.Split(arr[1], "")
+		arr3 := strings.Split(arr[2], "")
+		differentArr := getDifferent(arr1, arr2, arr3)
+		
+		for _, value := range differentArr {
+			result += strings.Index(strings.Join(alphabet, ""), value)+1
+		}
+	} 
+	fmt.Println(result)
 }
