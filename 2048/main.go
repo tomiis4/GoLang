ec0e5bace05fcc91ed78e6a309730aac062d0f85
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-01-10 17:10:19 +0100

    + main loop, user-input, board

--- 2048/main.go
+++ 2048/main.go
@@ -1,1 +1,62 @@
- 
+package main
+
+import (
+	"fmt"
+)
+
+const boardSize int = 4 
+
+
+func getBoard() []int {
+	const boardVolume = boardSize * boardSize
+	board := []int{}
+	
+	for i:=0; i < boardVolume; i++ {
+		board = append(board, 0)
+	}
+	
+	return board
+}
+
+func printBoard(board []int) {
+	line := []int{}
+	
+	for i, elem := range board {
+		line = append(line, elem)
+		
+		if i%boardSize == boardSize-1{
+			fmt.Println(line)
+			line = []int{}
+		}
+	}
+}
+
+func getInput() {
+	var keyPressed string
+	
+	_, err := fmt.Scan(&keyPressed)
+	
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(keyPressed)
+	}
+}
+
+func main() {
+	// Variables
+	board := getBoard()
+	highScore := 0
+	
+	// game loop
+	for ;; {
+		// game info
+		fmt.Println("\n \n \n ")
+		fmt.Println("2048")
+		fmt.Println("Higest score: ", highScore)
+		
+		// game functions
+		printBoard(board)
+		getInput()
+	}
+}
