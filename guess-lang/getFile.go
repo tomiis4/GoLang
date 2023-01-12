bc9dd622804b3393e46be9a7ffa81bc127acd47e
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-01-12 21:39:54 +0100

    + new file, for getting code from repos

---
+++ guess-lang/getFile.go
@@ -0,0 +1,15 @@
+package getFile
+
+import (
+	"fmt"
+)
+
+// url
+// https://api.github.com/search/repositories?q=language:typescript&sort=stars
+// -> tree -> content
+//
+// or header 
+// Accept: 'application/vnd.github+json'
+func getFile() {
+	fmt.Println("Test")
+}
