a9bee16c1cecd866cdae265fa549440284f5ed71
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-01-12 20:31:50 +0100

    + game-loop, fix some bugs

--- guess-lang/main.go
+++ guess-lang/main.go
@@ -39,6 +39,23 @@ func randInt(max int) int {
 	return randomNum
 }
 
+func delay(ms time.Duration) {
+	time.Sleep(ms * time.Millisecond)
+}
+
+func getLine(strArr []string) string {
+	//FIXME need fix tho
+	letterLen := len(strings.Split(strings.Join(strArr, " "), "")) *2+3
+	line := []string{}
+	
+	
+	for i:=0; i < letterLen; i++ {
+		line = append(line, "-")
+	}
+	
+	return strings.Join(line, "")
+}
+
 func displayCode(codeBlock []string, level uint8) {
 	for i:=uint8(0); i < uint8(len(codeBlock)); i++ {
 		elem := codeBlock[i]
@@ -53,7 +70,7 @@ func displayCode(codeBlock []string, level uint8) {
 	}
 }
 
-func displayLanguages(current string) {
+func getLanguages(current string) []string {
 	languagesChoices := 4
 	languages := [17]string{"Java", "C", "Python", "C++", "C#", "P*P", "JavaScript", "SQL", "Ruby", "Matlab", "Swift", "Go", "COBOL", "Fortran", "Rust", "Haskell", "Bash"}
 	selectedLanguages := []string{}
@@ -74,32 +91,47 @@ func displayLanguages(current string) {
 	// insert correct language
 	selectedLanguages[randInt(languagesChoices)] = current
 	
-	fmt.Println(selectedLanguages)
+	return selectedLanguages
 }
 
-func displayUI(score uint16, thisScore uint8, codeBlock []string, codeLang string) {
-	const strSpace string = "------------------------------------"
+func displayLanguages(languages []string) {
+	for i, elem := range languages {
+		if i == 0 { fmt.Printf("|") }
+		fmt.Printf(" %d. %s |", i+1, elem)
+		
+		if i+1 == len(languages) { fmt.Printf("\n") }
+	}
+}
+
+func displayUI(score uint16, questionScore uint8, code []string, codeRevaled uint8, languages []string) {
+	strSpace := getLine(languages)	
 	
 	fmt.Println(strSpace)
-	
+	// score
 	fmt.Println("Total score:", score)
-	fmt.Println("Current score:", thisScore)
-	
+	fmt.Println("Current score:", questionScore)
 	fmt.Println(strSpace)
-	
-	displayCode(codeBlock, 2)
-	
+	// code
+	displayCode(code, codeRevaled)
 	fmt.Println(strSpace)
-	
-	displayLanguages(codeLang)
-	
+	// languages
+	displayLanguages(languages)
 	fmt.Println(strSpace)
 }
 
 func main() {
-	fmt.Println("Hello, world")
-	
 	code := []string{"#include <stdio.h>", "int main(", "  printf(Helloworld);", "  return 0;"} 
+	codeRevaled := uint8(0)
+	currentLang := "C"
+	languages := getLanguages(currentLang)
+	score := uint16(2000)
+	questionScore := uint8(250)
 	
-	displayUI(1800, 225, code, "C")
+	
+	for ;; {
+		displayUI(score, questionScore, code, codeRevaled, languages)
+		codeRevaled++
+		delay(1500)
+	}
+	
 }
