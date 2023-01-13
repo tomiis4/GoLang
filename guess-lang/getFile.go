227596541e329f74834f12e9bdb2c7c7bcf67d27
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-01-13 22:23:00 +0100

    + get random repo, trying get content

--- guess-lang/getFile.go
+++ guess-lang/getFile.go
@@ -1,15 +1,91 @@
-package getFile
+package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
 )
 
-// url
-// https://api.github.com/search/repositories?q=language:typescript&sort=stars
-// -> tree -> content
-//
-// or header 
-// Accept: 'application/vnd.github+json'
-func getFile() {
-	fmt.Println("Test")
+type Repos struct {
+    Items []Repo `json:"items"`
+}
+
+type Repo struct {
+    Name string `json:"name"`
+    Description string `json:"description"`
+    URL string `json:"html_url"`
+}
+
+
+// type Files struct {
+//     Items []File
+// }
+
+// type File struct {
+//     Name string `json:"name"`
+//     ContentUrl string `json:"download_url"`
+// }
+type File struct {
+    Name string `json:"name"`
+}
+
+func randInt(max int) int {
+	 randTime := rand.NewSource(time.Now().UnixNano())
+	 newRand := rand.New(randTime)
+	 randomNum := newRand.Intn(max)
+    	
+	 return randomNum
+}
+
+func getRepoUrl(lang string) string {
+    link := "https://api.github.com/search/repositories?q=language:"+ lang +"&sort=stars"
+    
+    // send req.
+    resp, err := http.Get(link)
+    if err != nil { fmt.Println(err) }
+    defer resp.Body.Close()
+    
+    // get repos from req. and convert it
+    var repositories Repos
+    json.NewDecoder(resp.Body).Decode(&repositories)
+    
+    // make sure there is at least 1 repo
+    if len(repositories.Items) == 0 { return ""}
+    
+    // get repo
+    randomRepo := repositories.Items[randInt(len(repositories.Items))]
+    repoUrl := randomRepo.URL
+    
+    // log it
+    fmt.Println("name:", randomRepo.Name)
+    fmt.Println("url:", repoUrl)
+    
+    return repoUrl
+}
+
+func getFile(url string) {
+    nameUrl := strings.Replace(url, "https://github.com/", "", 1)
+    repoUrl := "https://api.github.com/repos/" + nameUrl + "/contents"
+    
+    // send req.
+    resp, err := http.Get(repoUrl)
+    if err != nil { fmt.Println(err) }
+    defer resp.Body.Close()
+    
+    // get repos from req. and convert it
+    var files []File
+    json.NewDecoder(resp.Body).Decode(&files)
+    
+    // make sure there is at least 1 repo
+    if len(files) == 0 { fmt.Println("No items were found") }
+    
+    fmt.Println(files)
+}
+
+func main() { 
+    repoUrl := getRepoUrl("typescript")
+    getFile(repoUrl)
 }
