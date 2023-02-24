869614e0db3f022069c91689686a710dbe2377ef
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-02-24 21:38:25 +0100

    + print table

--- cli/count-lines/main.go
+++ cli/count-lines/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -23,15 +24,45 @@ func repeat(n int, item string) string {
 }
 
 func t_print(arr []LinesCount) {
-	fmt.Println(repeat(10, "-"))
+	top_str := "Language | Lines | Blank | Comments"
 
-	for _, value := range arr {
-		fmt.Println(value)
+	fmt.Println(repeat(len(top_str), "-"))
+	fmt.Println(top_str)
+	fmt.Println(repeat(len(top_str), "-"))
+
+	for _, elem := range arr {
+		lang_len := len(elem.language)
+		lines_len := len(strconv.Itoa(elem.lines))
+		blank_len := len(strconv.Itoa(elem.blank))
+
+		lang_spaces := repeat(11-lang_len, " ")
+		format_lang_lines := fmt.Sprintf("%s%s%d", elem.language,lang_spaces , elem.lines)
+
+		blank_spaces := repeat(19-(lines_len+lang_len+len(lang_spaces)), " ")
+		format_blank := fmt.Sprintf("%s%d", blank_spaces, elem.blank)
+
+		comment_spaces := repeat(27-(blank_len+lines_len+lang_len+len(lang_spaces)+len(blank_spaces)), " ")
+		format_command := fmt.Sprintf("%s%d", comment_spaces, elem.comments)
+
+		formated_str := fmt.Sprintf("%s%s%s", format_lang_lines, format_blank, format_command)
+		fmt.Println(formated_str)
 	}
 
-	fmt.Println(repeat(10, "-"))
+	fmt.Println(repeat(len(top_str), "-"))
 }
 
 func main() {
-	fmt.Println("Hello")
+	var obj LinesCount
+	obj.language = "GoLang"
+	obj.comments = 51
+	obj.blank = 240
+	obj.lines = 197
+
+	var obj2 LinesCount
+	obj2.language = "CPP"
+	obj2.comments = 11
+	obj2.blank = 512
+	obj2.lines = 19823
+
+	t_print([]LinesCount{obj, obj2})
 }
